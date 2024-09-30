441109753e8096358c54881c4712ed84e9bd1a70
Gatuzo_CXL <tkmlc29@gmail.com>
2024-09-29 23:46:21 -0500

    Lab08AplMov - modificaciones3

--- app/src/main/java/com/example/lab08/TaskDatabase.kt
+++ app/src/main/java/com/example/lab08/TaskDatabase.kt
@@ -2,20 +2,26 @@ package com.example.lab08
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.room.migration.Migration //nuevas importaciones
+import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
 
-
-@Database(entities = [Task::class], version = 2) //modificacion a veriosn 2
+@Database(entities = [Task::class], version = 2) //modificacion a version 2
 abstract class TaskDatabase : RoomDatabase() {
     abstract fun taskDao(): TaskDao
 
-//migraciones
-    companion object{
+    companion object {
         val MIGRATION_1_2 = object : Migration(1, 2) {
             override fun migrate(database: SupportSQLiteDatabase) {
-                database.execSQL(
-                    "CREATE TABLE IF NOT EXISTS `Task` (`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `description` TEXT NOT NULL, `priority` INTEGER NOT NULL DEFAULT 1, `category` TEXT NOT NULL DEFAULT '')")
+                database.execSQL("""
+                    CREATE TABLE IF NOT EXISTS `tasks` (
+                        `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                        `description` TEXT NOT NULL,
+                        `is_completed` INTEGER NOT NULL DEFAULT 0,
+                        `priority` INTEGER NOT NULL DEFAULT 1,
+                        `category` TEXT NOT NULL DEFAULT '',
+                        `created_at` INTEGER NOT NULL DEFAULT 0
+                    )
+                """.trimIndent())
             }
         }
     }
